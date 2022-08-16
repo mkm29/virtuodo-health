e9f08bec33f34a798198832c57588906456dc6d0
Mitch Murphy Murphy <mitch.murphy@gmail.com>
2022-08-15 22:14:47 -0400

    Updated logic in main.go to handle erros better and structure response better; added sample deployment manifest

--- main.go
+++ main.go
@@ -12,27 +12,49 @@ import (
 )
 
 const (
-	virtDir         = "/opt/virtuoso-opensource"
-	memoryThreshold = 20
+	virtDir          = "/opt/virtuoso-opensource"
+	memoryThreshold  = 20
+	diskThreshold    = 20
+	buffersThreshold = 20
 )
 
 // create struct to hold Virtuoso status
 // TODO: if you want this to be a livenessProbe, need to add a bool field
 type Status struct {
-	TotalBuffers       int
-	UsedBuffers        int
-	DirtyBuffers       int
-	FreeBuffers        int
-	PercentBuffersFree float64
-	MemoryTotal        int
-	MemoryUsed         int
-	MemoryFree         int
-	MemoryCache        int
-	MemoryPercentFree  float64
-	DiskUsed           int
-	DiskFree           int
-	PercentDiskUsed    float64
-	Status             bool
+	Memory struct {
+		Free        int
+		Used        int
+		Total       int
+		Cache       int
+		Percent     float64
+		PercentFree float64
+		Status      bool
+	}
+	Disk struct {
+		Used        int
+		Free        int
+		Percent     float64
+		PercentFree float64
+		Status      bool
+	}
+	Buffers struct {
+		Used        int
+		Free        int
+		Percent     float64
+		PercentFree float64
+		Status      bool
+	}
+	Virtuoso struct {
+		Buffers struct {
+			Total       int
+			Used        int
+			Free        int
+			Dirty       int
+			Percent     float64
+			PercentFree float64
+		}
+		Status bool
+	}
 }
 
 func (s *Status) getVirtuosoStats() (bool, error) {
@@ -46,16 +68,25 @@ func (s *Status) getVirtuosoStats() (bool, error) {
 
 	var bre = regexp.MustCompile(`(\d{1,}) buffers`)
 	rs := bre.FindStringSubmatch(string(out))
-
-	s.TotalBuffers, _ = strconv.Atoi(rs[1])
+	if len(rs) > 1 {
+		s.Virtuoso.Buffers.Total, _ = strconv.Atoi(rs[1])
+	}
 	var bure = regexp.MustCompile(`, (\d{1,}) used`)
 	rs2 := bure.FindStringSubmatch(string(out))
-	s.UsedBuffers, _ = strconv.Atoi(rs2[1])
+	if len(rs2) > 1 {
+		s.Virtuoso.Buffers.Used, _ = strconv.Atoi(rs2[1])
+	}
 	var dbre = regexp.MustCompile(`, (\d{1,}) dirty`)
 	rs3 := dbre.FindStringSubmatch(string(out))
-	s.DirtyBuffers, _ = strconv.Atoi(rs3[1])
-	// compute percent of buffers free
-	s.PercentBuffersFree = float64(s.TotalBuffers-s.UsedBuffers) / float64(s.TotalBuffers) * 100
+	if len(rs3) > 1 {
+		s.Virtuoso.Buffers.Free, _ = strconv.Atoi(rs3[1])
+	}
+	// if total and free buffers are set, calculate percent free
+	if s.Virtuoso.Buffers.Used > 0 && s.Virtuoso.Buffers.Total > 0 {
+		s.Virtuoso.Buffers.PercentFree = float64(s.Virtuoso.Buffers.Total-s.Virtuoso.Buffers.Used) / float64(s.Virtuoso.Buffers.Total) * 100
+		s.Virtuoso.Buffers.Percent = 100 - s.Virtuoso.Buffers.PercentFree
+	}
+	s.Virtuoso.Status = s.Virtuoso.Buffers.PercentFree < buffersThreshold
 	return true, nil
 }
 
@@ -68,13 +99,18 @@ func (s *Status) getDiskUsage() (bool, error) {
 	}
 	// split by spaces
 	df := strings.Split(string(out), " ")
+	if len(df) < 3 {
+		return false, fmt.Errorf("Error: df output has %d fields", len(df))
+	}
 
 	diskUsed, _ := strconv.Atoi(df[2])
 	diskFree, _ := strconv.Atoi(df[3])
 	percentDiskUsed := float64(diskUsed) / (float64(diskFree) + float64(diskUsed)) * 100
-	s.DiskUsed = diskUsed
-	s.DiskFree = diskFree
-	s.PercentDiskUsed = percentDiskUsed
+	s.Disk.Used = diskUsed
+	s.Disk.Free = diskFree
+	s.Disk.PercentFree = percentDiskUsed
+	s.Disk.Percent = 100 - percentDiskUsed
+	s.Disk.Status = s.Disk.Percent > diskThreshold
 	return true, nil
 }
 
@@ -87,11 +123,16 @@ func (s *Status) getMemoryUsage() (bool, error) {
 	}
 	// split by spaces
 	mem := strings.Split(string(out), " ")
-	s.MemoryFree, _ = strconv.Atoi(mem[3])
-	s.MemoryUsed, _ = strconv.Atoi(mem[2])
-	s.MemoryTotal, _ = strconv.Atoi(mem[1])
-	s.MemoryCache, _ = strconv.Atoi(mem[5])
-	s.MemoryPercentFree = float64(s.MemoryFree) / float64(s.MemoryTotal) * 100
+	if len(mem) < 6 {
+		fmt.Printf("Error: free output has %d fields: %v", len(mem), mem)
+		return false, nil
+	}
+	s.Memory.Free, _ = strconv.Atoi(mem[3])
+	s.Memory.Used, _ = strconv.Atoi(mem[2])
+	s.Memory.Total, _ = strconv.Atoi(mem[1])
+	s.Memory.Cache, _ = strconv.Atoi(mem[5])
+	s.Memory.PercentFree = float64(s.Memory.Free) / float64(s.Memory.Total) * 100
+	s.Memory.Status = s.Memory.PercentFree < memoryThreshold
 	return true, nil
 }
 
@@ -110,29 +151,30 @@ func getStatus(w http.ResponseWriter, r *http.Request) {
 
 	// DISK USAGE
 	ok, err = status.getDiskUsage()
-	if !ok {
-		fmt.Printf("Error: %s\n", err)
-		return
+	if err != nil {
+		fmt.Printf("Disk Error: %s\n", err)
+		// return
 	}
 	// MEMORY USAGE
 	ok, err = status.getMemoryUsage()
-	if !ok {
-		fmt.Printf("Error: %s\n", err)
-		return
+	if err != nil {
+		fmt.Printf("Memory Error: %s\n", err)
+		// return
 	}
-	if status.MemoryPercentFree < memoryThreshold {
-		fmt.Printf("Memory usage is %.2f%%\n", status.MemoryPercentFree)
-		status.Status = false
+	if status.Memory.PercentFree < memoryThreshold {
+		fmt.Printf("Memory usage is %.2f%%\n", status.Memory.PercentFree)
+		status.Memory.Status = false
 	} else {
-		status.Status = true
+		status.Memory.Status = true
 	}
+	fmt.Printf("Status at %s: %v\n", time.Now(), status)
 
 	json.NewEncoder(w).Encode(status)
 }
 
 func main() {
 	fmt.Printf("Starting Virtuoso status server at %s\n", time.Now())
-	http.HandleFunc("/", getStatus)
+	http.HandleFunc("/health", getStatus)
 
 	err := http.ListenAndServe(":3333", nil)
 	if err != nil {
